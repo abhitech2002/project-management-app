606bd1c4a182b57376a4941a52f2372b3b16c67b
abhitech2002 <oabhishek31@gmail.com>
2024-10-10 00:05:29 +0530

    feat: add user login routes

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -40,6 +40,83 @@ const registerUser = asyncHandler(async (req, res) => {
         .json(new ApiResponse(200, createdUser, "User registered successfully."));
 });
 
+const generateAccessAndRefreshTokens = async (userId) => {
+    try {
+        const user = await User.findById(userId);
+        const accessToken = user.generateAccessToken();
+        const refreshToken = user.generateRefreshToken();
+
+        user.refreshToken = refreshToken;
+
+        await user.save({ validateBeforeSave: false });
+
+        return { accessToken, refreshToken };
+    } catch (error) {
+        throw new ApiError(
+            500,
+            "Something went wrong while generating refresh and access token"
+        );
+    }
+};
+
+const loginUser = asyncHandler(async (req, res, next) => {
+    try {
+        const { email, username, password } = req.body;
+
+        if (!username && !email) {
+            return next(new ApiError(400, "User name or email is required"));
+        }
+
+        const user = await User.findOne({
+            $or: [{ username }, { email }],
+        });
+
+        if (!user) {
+            return next(new ApiError(404, "User doesn't exist"));
+        }
+
+        const isPasswordValid = await user.isPasswordCorrect(password);
+
+        if (!isPasswordValid) {
+            return next(new ApiError(401, "Invalid user credentials"));
+        }
+
+        const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(
+            user._id
+        );
+
+        const loggedInUser = await User.findById(user._id).select(
+            "-password -refreshToken"
+        );
+
+        const options = {
+            httpOnly: true,
+            secure: true,
+        };
+
+        return res
+            .status(200)
+            .cookie("accessToken", accessToken, options)
+            .cookie("refreshToken", refreshToken, options)
+            .json(
+                new ApiResponse(
+                    200,
+                    {
+                        user: loggedInUser,
+                        accessToken,
+                        refreshToken,
+                    },
+                    "User logged In Successfully"
+                )
+            );
+    } catch (error) {
+        return next(
+            new ApiError(500, "Something went wrong while logging in user")
+        );
+    }
+});
+
 export {
-    registerUser
+    registerUser,
+    loginUser
 }
